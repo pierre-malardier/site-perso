ab2dd9f1409eb696f373ef717f1970798f0fd954
pierre-malardier <pierre.malardier@laplateforme.io>
2021-03-19 15:57:09 +0100

    mise en place du header section competences et section projets

--- index.php
+++ index.php
@@ -5,18 +5,77 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Hello Bulma!</title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.1/css/bulma.min.css">
+    <link href="style.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link href="style.css" rel="stylesheet">
 </head>
 
 <body>
-    <section class="section">
-        <div class="container">
-            <h1 class="title">
-                Hello World
-            </h1>
-            <p class="subtitle">
-                My first website with <strong>Bulma</strong>!
-            </p>
+    <header>
+        <div class=" black">
+            <a class="grey-text" href=""><i class="small material-icons">mail</i>pierremalardier1991@gmail.com</a>
+            <a class="grey-text" href=""><i class="small material-icons">phone</i>0788856629</a>
+        </div>
+        <nav class="white">
+            <ul>
+                <li><a href="#"><img src="./img/logo2_pierre.png" width="250"></a></li>
+                <li><a class="black-text" href="#">Mes projets</a></li>
+                <li><a class="black-text" href="#">CV</a></li>
+            </ul>
+        </nav>
+
+    </header>
+    <main>
+    </main>
+    <section>
+        <p class="center-align">
+            <a class="waves-effect waves-light btn black ">Mes compétences</a>
+        </p>
+        <div class="row">
+            <img class="col s2" src="./img/logo html.png" width="150">
+            <img class="col s2" src="./img/logo-css.png" width="150">
+            <img class="col s2" src="./img/logo-php.png" width="150">
+            <img class="col s2" src="./img/MySQL-Logo.png" width="150">
+        </div>
+    </section>
+    <section>
+        <p class="center-align">
+            <a class="waves-effect waves-light btn black ">Mes projets</a>
+        </p>
+        <div class="row">
+            <a href="#">
+                <div class="col s6 ">
+                    <div class="card">
+                        <div class="card-image">
+                            <img src="./img/discussion.PNG">
+                            <span class="card-title">Discussion</span>
+                        </div>
+                        <div class="card-content">
+                            <p>Ce projet est un site sur lequel j'ai crée un module de discussion.</p>
+                        </div>
+                        <div class="card-action center-align ">
+                            <a class="black-text " href="#">Voir le projet</a>
+                        </div>
+                    </div>
+            </a>
+        </div>
+        <div class="col s6 ">
+            <a href="#">
+                <div class="card">
+                    <div class="card-image">
+                        <img src="./img/blog.PNG">
+                        <span class="card-title">Blog</span>
+                    </div>
+                    <div class="card-content">
+                        <p>Réalisation d'un blog avec page admin et possibilité d'ajouter effacer ou modifier des articles.</p>
+                    </div>
+                    <div class="card-action center-align ">
+                        <a class="black-text" href="#">Voir le projet</a>
+                    </div>
+                </div>
+            </a>
+        </div>
         </div>
     </section>
 </body>
