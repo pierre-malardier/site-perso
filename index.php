28b78cdf7a29a86872b05f768584f2d42e158413
pierre-malardier <pierre.malardier@laplateforme.io>
2021-04-12 15:03:23 +0200

    switch from materialize to boostrap

--- index.php
+++ index.php
@@ -6,108 +6,128 @@
     <meta lang="fr">
     <meta name="keywords" content="Pierre Malardier, La plateforme_, dév, web,">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="Pierre Malardier Dév en devenir à la Plateforme_ à la Coding School, l’école de code, ce sont 2 années dédiées à la formation aux métiers du web et du mobile, deux titres reconnus par l’état et un modèle pédagogique unique qui s’adapte aux besoins de chacun.
- " />
+    <meta name="description" content="Pierre Malardier Dév en devenir à la Plateforme_ à la Coding School, l’école de code, ce sont 2 années dédiées à la formation aux métiers du web et du mobile, deux titres reconnus par l’état et un modèle pédagogique unique qui s’adapte aux besoins de chacun." />
     <title>Pierre Malardier</title>
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
     <link href="style.css" rel="stylesheet">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
-    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-    <link href="style.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css">
     <link rel="icon" type="image" href="./ressource/img/icon.png" alt="icon pm" />
 </head>
 
 <body>
     <header>
-        <div class="top_nav black row ">
-            <a class="grey-text center col-s12 m6 13" href="mailto:pierremalardier1991@gmail.com"><i class="small material-icons">mail</i>pierremalardier1991@gmail.com</a>
-            <a class="grey-text col-s12 m6 13" href="tel:+33788856629"><i class="small material-icons">phone</i>+33(0)788856629</a>
-            <div class="col s6 offset-s6"><span class="flow-text"></span></div>
-            <a class="col-s12 m6 13" href="https://www.linkedin.com/in/pierre-malardier-8a4b10124/"><img src="./ressource/img/linkedin.png" alt="logo linkedin"></a>
-            <a class="col-s12 m6 13" href="https://github.com/pierre-malardier"><img src="./ressource/img/GitHub.png" alt="logo github"></a>
-        </div>
-        <nav class="white">
-            <div class="nav-wrapper grey">
-                <a href="#" class="brand-logo"><img src="./ressource/img/logo2_pierre .png" width="70%" alt="logo Pierre Malardier"></a>
-                <ul id="nav-mobile" class="right hide-on-med-and-down">
-                    <li><a class="black-text" href="#compétences">Mes compétences</a></li>
-                    <li><a class="black-text" href="#projets">Mes projets</a></li>
-                    <li><a id="cv" class="black-text" href="./ressource/pierre-malardier-cv.pdf">CV</a></li>
-                </ul>
-            </div>
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+            <a href="#" class="navbar-brand"><img src="./ressource/img/logo_pierre.png" alt="logo Pierre Malardier"></a>
+            <div class="container">
+                <div class="navbar-header">
+                    <a type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbarSupportedContent">
+                        <span class="sr-only"><img src="./ressource/img/menu.png" alt="burger menu"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </a>
+                </div>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <a href="#myModal" role="button" data-toggle="modal" class="nav-link">Mes Compétences</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#projets">Mes projets</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="./ressource/pierre-malardier-cv.pdf">CV</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="https://www.linkedin.com/in/pierre-malardier-8a4b10124/"><img class="logo-social" src="./ressource/img/linkedin.png" alt="logo linkedin"></a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="https://github.com/pierre-malardier"><img class="logo-social" src="./ressource/img/GitHub.png" alt="logo github"></a>
+                        </li>
+                    </ul>
+                </div>
         </nav>
-
     </header>
     <main>
+        <div class="description d-flex">
+            <div>
+                <p class="d-flex justify-content-center"><img src="./ressource/img/moi.png" alt="photo portrait"></p>
+
+                <h1 class="d-flex justify-content-center m-2">Développeur Web Full Stack en devenir !!!</h1>
+                <p class="d-flex justify-content-center m-5"><strong>Actuellement en formation à la PLateforme_, je suis à la recherche d'une alternance pour septembre 2021 pour
+                        continuer ma deuxième année à la PLateforme_.</strong>
+                </p>
+            </div>
+
+        </div>
+
     </main>
-    <section>
-        <balise id="compétences"></balise>
-        <p class="center-align">
-            </a><a class="waves-effect waves-light btn black ">Mes compétences</a>
-        </p>
-        <div class="center-align">
-            <img class="" src="./ressource/img/langage-web-removebg-preview.png" width="80%" alt="logo langage">
-        </div>
-    </section>
-    <section class="black card-list">
-        <balise id="projets"></balise>
-        <p class="center-align">
-            <a class="waves-effect waves-light btn black ">Mes projets</a>
-        </p>
-        <div class="row">
-            <a href="#">
-                <div class="col s4 ">
-                    <div class="card">
-                        <div class="card-image">
-                            <img src="./ressource/img/discussion.PNG" alt="projet discussion">
-                            <span class="card-title">Discussion</span>
-                        </div>
-                        <div class="card-content">
-                            <p>Ce projet est un site sur lequel j'ai crée un module de discussion.</p>
-                        </div>
-                        <div class="card-action center-align ">
-                            <a class="black-text " href="#">Voir le projet</a>
-                        </div>
+    <div class="bs-example">
+        <div id="myModal" class="modal fade" tabindex="-1">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title">Mes Compétences</h5>
+                        <a type="button" class="close text-dark" data-dismiss="modal"><i class="bi bi-x-circle-fill"></i></a>
                     </div>
-            </a>
-        </div>
-        <div class="col s4 ">
-            <a href="./projets/blog/index.php">
-                <div class="card">
-                    <div class="card-image">
-                        <img src="./ressource/img/blog.PNG" alt="projet blog">
-                        <span class="card-title">Blog</span>
-                    </div>
-                    <div class="card-content">
-                        <p>Réalisation d'un blog avec page admin et possibilité d'ajouter effacer ou modifier des articles.</p>
-                    </div>
-                    <div class="card-action center-align ">
-                        <a class="black-text" href="./projets/blog/index.php">Voir le projet</a>
+                    <div class="modal-body">
+                        <div class="row">
+                            <p class="col"><img src="./ressource/img/html.png" alt="logo hmtl"></p>
+                            <p class="col"><img src="./ressource/img/css.png" alt="logo css"></p>
+                            <p class="col"><img src="./ressource/img/js.png" alt="logo js"></p>
+                        </div>
+                        <div class="row">
+                            <p class="col"><img src="./ressource/img/php.png" alt="logo php"></p>
+                            <p class="col"><img src="./ressource/img/github2.png" alt="logo github"></p>
+                            <p class="col"><img src="./ressource/img/bootstrap.png" alt="logo bootstrap"></p>
+                        </div>
+                        <div class="row">
+                            <p class="col"><img src="./ressource/img/materialize.png" alt="logo materialize"></p>
+                            <p class="col"><img src="./ressource/img/figma.png" alt="logo figma"></p>
+                            <p class="col"><img src="./ressource/img/adobexd.png" alt="logo adobe xd"></p>
+                        </div>
                     </div>
                 </div>
-            </a>
+            </div>
         </div>
-        <div class="col s4 ">
-            <a href="#">
-                <div class="card">
-                    <div class="card-image">
-                        <img src="./ressource/img/memory.PNG" alt="projet memory">
-                        <span class="card-title">Memory</span>
-                    </div>
-                    <div class="card-content">
-                        <p>Réalisation du jeu Memory.</p>
-                    </div>
-                    <div class="card-action center-align ">
-                        <a class="black-text" href="#">Voir le projet</a>
-                    </div>
-                </div>
-            </a>
+    </div>
+    <h2 class="d-flex justify-content-center">Mes projets</h2>
+    <section class="d-flex justify-content-around">
+
+        <div class="card m-5" style="width: 18rem;">
+            <img src="./ressource/img/discussion.PNG" alt="projet discussion">
+            <div class="card-body">
+                <h5 class="card-title">Discussion</h5>
+                <p class="card-text">Ce projet est un site sur lequel j'ai crée un module de discussion.</p>
+                <a class="btn" href="#">Voir le projet</a>
+            </div>
         </div>
+        <div class="card m-5" style="width: 18rem;">
+            <img src="./ressource/img/blog.PNG" alt="projet blog">
+            <div class="card-body">
+                <h5 class="card-title">Blog</h5>
+                <p class="card-text">Réalisation d'un blog avec page admin et possibilité d'ajouter effacer ou modifier des articles.</p>
+                <a class="btn" href="#">Voir le projet</a>
+            </div>
+        </div>
+        <div class="card m-5" style="width: 18rem;">
+            <img src="./ressource/img/memory.PNG" alt="projet memory">
+            <div class="card-body">
+                <h5 class="card-title">Memory</h5>
+                <p class="card-text">Réalisation du jeu Memory.</p>
+                <a class="btn" href="#">Voir le projet</a>
+            </div>
         </div>
         <div class="center-align">
             <a href="#"><img src="./ressource/img/haut.png" width="10%" alt="fleche haut"></a>
         </div>
     </section>
-    <script type="text/javascript" src="script.js"></script>
+    <footer class="bg-dark fixed-bottom">
+        <p class=" d-flex text-light justify-content-center">© 2021 Copyright : Pierre Malardier </p>
+    </footer>
 </body>
 
 </html>
