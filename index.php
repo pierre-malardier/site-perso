7595e661aca1bd79f70276bdee15fba8b3338184
pierre-malardier <pierre.malardier@laplateforme.io>
2021-04-12 09:13:38 +0200

    meta and linkedin

--- index.php
+++ index.php
@@ -3,26 +3,31 @@
 
 <head>
     <meta charset="utf-8">
+    <meta lang="fr">
+    <meta name="keywords" content="Pierre Malardier, La plateforme_, dév, web,">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="Pierre Malardier Dév en devenir à la Plateforme_ Marseille." />
+    <meta name="description" content="Pierre Malardier Dév en devenir à la Plateforme_ à la Coding School, l’école de code, ce sont 2 années dédiées à la formation aux métiers du web et du mobile, deux titres reconnus par l’état et un modèle pédagogique unique qui s’adapte aux besoins de chacun.
+ " />
     <title>Pierre Malardier</title>
     <link href="style.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <link href="style.css" rel="stylesheet">
-    <link rel="icon" type="image" href="./ressource/img/icon.png" />
+    <link rel="icon" type="image" href="./ressource/img/icon.png" alt="icon pm" />
 </head>
 
 <body>
     <header>
-        <div class="top_nav black">
-            <a class="grey-text center" href="mailto:pierremalardier1991@gmail.com"><i class="small material-icons">mail</i>pierremalardier1991@gmail.com</a>
-            <a class="grey-text" href="tel:+33788856629"><i class="small material-icons">phone</i>+33(0)788856629</a>
-            <a class=""><img src=""></a>
+        <div class="top_nav black row ">
+            <a class="grey-text center col-s12 m6 13" href="mailto:pierremalardier1991@gmail.com"><i class="small material-icons">mail</i>pierremalardier1991@gmail.com</a>
+            <a class="grey-text col-s12 m6 13" href="tel:+33788856629"><i class="small material-icons">phone</i>+33(0)788856629</a>
+            <div class="col s6 offset-s6"><span class="flow-text"></span></div>
+            <a class="col-s12 m6 13" href="https://www.linkedin.com/in/pierre-malardier-8a4b10124/"><img src="./ressource/img/linkedin.png" alt="logo linkedin"></a>
+            <a class="col-s12 m6 13" href="https://github.com/pierre-malardier"><img src="./ressource/img/GitHub.png" alt="logo github"></a>
         </div>
         <nav class="white">
             <div class="nav-wrapper grey">
-                <a href="#" class="brand-logo"><img src="./ressource/img/logo2_pierre .png" width="70%"></a>
+                <a href="#" class="brand-logo"><img src="./ressource/img/logo2_pierre .png" width="70%" alt="logo Pierre Malardier"></a>
                 <ul id="nav-mobile" class="right hide-on-med-and-down">
                     <li><a class="black-text" href="#compétences">Mes compétences</a></li>
                     <li><a class="black-text" href="#projets">Mes projets</a></li>
@@ -33,13 +38,6 @@
 
     </header>
     <main>
-        <article class="description">
-            <h1> Web développeur en formation à la Plateforme_</h1>
-            <p>
-                Actuellement en formation à la Plateforme _ , je suis à la recherche d'une alternance pour ma deuxiéme année de formation pour la rentrée de septembre 2021.
-
-            </p>
-        </article>
     </main>
     <section>
         <balise id="compétences"></balise>
@@ -47,7 +45,7 @@
             </a><a class="waves-effect waves-light btn black ">Mes compétences</a>
         </p>
         <div class="center-align">
-            <img class="" src="./ressource/img/langage-web-removebg-preview.png" width="80%">
+            <img class="" src="./ressource/img/langage-web-removebg-preview.png" width="80%" alt="logo langage">
         </div>
     </section>
     <section class="black card-list">
@@ -60,7 +58,7 @@
                 <div class="col s4 ">
                     <div class="card">
                         <div class="card-image">
-                            <img src="./ressource/img/discussion.PNG">
+                            <img src="./ressource/img/discussion.PNG" alt="projet discussion">
                             <span class="card-title">Discussion</span>
                         </div>
                         <div class="card-content">
@@ -76,7 +74,7 @@
             <a href="./projets/blog/index.php">
                 <div class="card">
                     <div class="card-image">
-                        <img src="./ressource/img/blog.PNG">
+                        <img src="./ressource/img/blog.PNG" alt="projet blog">
                         <span class="card-title">Blog</span>
                     </div>
                     <div class="card-content">
@@ -92,7 +90,7 @@
             <a href="#">
                 <div class="card">
                     <div class="card-image">
-                        <img src="./ressource/img/memory.PNG">
+                        <img src="./ressource/img/memory.PNG" alt="projet memory">
                         <span class="card-title">Memory</span>
                     </div>
                     <div class="card-content">
@@ -106,7 +104,7 @@
         </div>
         </div>
         <div class="center-align">
-            <a href="#"><img src="./ressource/img/haut.png" width="10%"></img></a>
+            <a href="#"><img src="./ressource/img/haut.png" width="10%" alt="fleche haut"></a>
         </div>
     </section>
     <script type="text/javascript" src="script.js"></script>
