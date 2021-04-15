007a010da53da215a633d0075bf421616e83de8a
pierre-malardier <pierre.malardier@laplateforme.io>
2021-04-15 11:45:53 +0200

    meta google

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 
 <head>
     <meta charset="utf-8">
+    <meta name="google-site-verification" content="HSUhPqoGgzfJLLk4FdJ6nD5yJHK1ZwTUaHXLf1b2ZRc" />
     <meta name="keywords" content="Pierre Malardier, La plateforme_, dév, web,">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="Pierre Malardier Dév en devenir à la Plateforme_ à la Coding School, l’école de code, ce sont 2 années dédiées à la formation aux métiers du web et du mobile, deux titres reconnus par l’état et un modèle pédagogique unique qui s’adapte aux besoins de chacun." />
